2323cec7363a291fc75b62eb75b9a5405ec71604
Ahmed Mufti <ahmed@mufti.dev>
2021-12-23 02:45:31 +0100

    fixed url

--- reservation.js
+++ reservation.js
@@ -2,37 +2,36 @@
 
 const puppeteer = require("puppeteer");
 
-const resources = new Map(
-                    ["301667", "592"], // seatID , seatNum
-                    ["301549", "474"],
-                    ["301662", "587"],
-                    ["301668", "593"],
-                    ["301697", "622"],
-);
+const resources = new Map([
+    ["301667", "592"], // seatID , seatNum
+    ["301549", "474"],
+    ["301662", "587"],
+    ["301668", "593"],
+    ["301697", "622"],
+]);
 
-
-function createDateObj (days) {
-        const date = new Date();
-        const newDate = date.setDate(date.getDate() + days);
-        return newDate;
+Date.prototype.addDays = function(days) {
+    let date = new Date(this.valueOf());
+    date.setDate(date.getDate() + days);
+    return date;
 }
 
+Date.prototype.getDateString = function () {
+    let date = new Date(this.valueOf());
+    return [date.getFullYear(), date.getMonth() + 1 , date.getDate()].join("-").toString();
+}
 
-function createNewReservation (seatID, seatNum, date, isMorning) {
+function createNewReservation (seatID, seatNum, days, isMorning) {
+    const date = new Date();
     const obj = {};
-    const dateStr = (dt) => [dt.getFullYear(), dt.getMonth(), dt.getDate()].join("-");
-    const startDate = dateStr(date);
-    const endDate = function (isMorning, date) {    
-        const dPlus1 = date.SetDate(date.getDate() + 1);
-        return isMorning ? startDate : dateStr(dPlus1); 
-    };
-    const startTime = (isMorning) => isMorning ? "12" : "17"; 
-    const endTime = (isMorning) => isMorning ? "17" : "00";
+    const startDate = date.addDays(days).getDateString();
+    const endDate = isMorning ? startDate : date.addDays(days + 1).getDateString();
+    const startTime = isMorning ? "12" : "17"; 
+    const endTime = isMorning ? "17" : "00";
     obj.seatNum = seatNum;
     obj.Url = `https://www-sso.groupware.kuleuven.be/sites/KURT/Pages/NEW-Reservation.aspx?StartDateTime=${startDate}T${startTime}:00:00&EndDateTime=${endDate}T${endTime}:00:00&ID=${seatID}`;
     return obj;
 }
-
 
 // Login 
 async function login (page, usr, pass) {
@@ -42,49 +41,54 @@ async function login (page, usr, pass) {
     const loginBtn = "#pwdLoginBtn";
     await page.waitForSelector(user);
     await page.evaluate((user, usr) => { 
-	    document.querySelector(user);
-	    inputUsr.value = usr;
+        const inputUsr = document.querySelector(user);
+        inputUsr.value = usr;
     }, user, usr);
     await page.waitForSelector(password);
     await page.evaluate((password, pass) => { 
-	const inputPass = document.querySelector(password);
-	inputPass.value = pass;
+        const inputPass = document.querySelector(password);
+        inputPass.value = pass;
     }, password, pass);    
     // click on login
     await page.click(loginBtn);
     await page.waitForNavigation({waitUntil: "networkidle0"});
 }
 
-async function makeReservation (page, isMorning) { 
+async function makeReservation (page, resources, days, isMorning, screen) { 
+    reservation_loop: 
     for (const [id, num] of resources) {
-        const instance = createNewReservation(id, num, date, isMorning);
-        await page.goto(instance.url);        
+        const instance = createNewReservation(id, num, days, isMorning);
+        console.log(instance.Url);
         await page.waitForNavigation({waitUntil: "networkidle0"});
+        await page.goto(instance.Url);        
+        await page.waitForNavigation();
         // fill submission form
-        await fillForm(page);
-        await submitForm(page, instance.seatNum);
+        const result = await submitForm(page, instance.seatNum);
+        if (result === 1) {
+            break reservation_loop; 
+        }
+        if (result === 0)
+        {
+            continue reservation_loop;
+        }
     }
 }
-    
-async function fillForm(page) {
-    const subject = "#kurtResourceSubject";
-    const checkbox = "#complyConditionsCheckbox";
-    await page.waitForSelector(subject, {timeout: 60000});
-    await page.waitForSelector(checkbox, {timeout: 60000});
-    // await page.$eval(subject, el => el.value = "Study");
-    await page.$eval(checkbox, el => el.checked = true);
-}
+
 
 async function submitForm (page, seatNum) {
+    const checkbox = "#complyConditionsCheckbox";
+    await page.waitForSelector(checkbox, {timeout: 60000});
+    await page.$eval(checkbox, el => el.checked = true);
     const submitBtn = "#submitReservationButton";
-    const confirmationMsg = "#";
+    const confirmationMsg = "#form-confirmation-message";
+    // const error = "#errorLabel .alert";
     await page.waitForSelector(submitBtn, {timeout: 60000});
     await page.click(submitBtn);
     // if confimation ==> break, else => continue 
-    const result = page
-                       .waitForSelector(confirmationMsg, {visible: true, timeout: 60000})
-                       .then(value => { console.log("Successfully booked seat:", seatNum); return break; 
-                        }, reason => { return continue; });
+    return page.
+        waitForSelector(confirmationMsg, {visible: true, timeout: 60000})
+        .then(value => { console.log("Successfully booked seat:", seatNum, value); return 1;
+        },reason => { console.log("Booking failed", reason); return 0; });
 }
 
 // deal with unhandled rejections
@@ -97,24 +101,26 @@ process.on("unhandledRejection", (reason, promise) => {
 async function main() {
     const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
     const screen = (page, name) =>  page.screenshot({ path: `${name}.png` });
-    const user = process.argv[2];
-    const password = process.argv[3];
+    const usr = process.argv[2];
+    const pass = process.argv[3];
     const dayPlus = Number(process.argv[4]);
-    // const isMorning = (process.argv[5] === "true");
-    const date = createDateObject(dayPlus);
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     // Change default timeout for navigation methods (goto, waitForNavigation etc.)
-    await page.setDefaultNavigationTimeout(60000);
+    page.setDefaultNavigationTimeout(60000);
     // Change default timeout 
-    await page.setDefaultTimeout(45000);
+    page.setDefaultTimeout(45000);
     // Set viewport width and height
     await page.setViewport({ width: 1280, height: 1800 });
     // Await login
     await login(page, usr, pass);
-    await makeReservation(page, false);
-    await makeReservation(page, true);
+    await screen(page, "login");
+    const evening = makeReservation(page, resources, dayPlus, false, screen);
+    await screen(page, "evening");
+    const morning = makeReservation(page, resources, dayPlus, true, screen);
+    const all = Promise.allSettled([evening, morning]);
+    all.then(() => console.log("Script executed with success!"));
     await browser.close();
 }
 
-
+main();
